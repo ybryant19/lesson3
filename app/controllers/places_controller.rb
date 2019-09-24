b20ebdb0895af6af5273320bf4fb4480945a9086
Yvette Bryant <bryant.yvette@gmail.com>
2019-09-24 02:43:58 +0000

    Added the submit button

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,9 +5,19 @@ class PlacesController < ApplicationController
   end
 
 
-def new
-  @place = Place.new
-end
+  def new
+    @place = Place.new
+  end
+ 
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
 
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
 
 end
