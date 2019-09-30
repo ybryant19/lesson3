d859f3d81217bf819d5d1ad40bc2d76e23f56923
Yvette Bryant <bryant.yvette@gmail.com>
2019-09-30 20:18:15 +0000

    Added form submission info

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root 'places#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-resources :places
+  resources :places do
+    resources :comments, only: :create
+  end
 end
