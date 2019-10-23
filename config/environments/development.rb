293b5307b70f6fb0ce8f35865ce0ecdaa5207f81
Yvette Bryant <bryant.yvette@gmail.com>
2019-10-23 05:24:06 +0000

    Triggering email when comments are added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,15 +1,18 @@
 Rails.application.configure do
-  config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "example.com",
-    autentication: "plain",
-    enable_starttles_auto: true,
-    user_name: ENV['GMAIL_ADDRESS'],
-    PASSWORD: ENV['GMAIL_PASSWORD']
-  }
+  config.action_mailer.deliver_method = :test
+  
+ # config.action_mailer.delivery_method = :smtp
+
+ # config.action_mailer.smtp_settings = {
+ #   address: "smtp.gmail.com",
+ #   port: 587,
+ #  domain: "example.com",
+ #   autentication: "plain",
+ #  enable_starttles_auto: true,
+ #   user_name: ENV['GMAIL_ADDRESS'],
+ #   PASSWORD: ENV['GMAIL_PASSWORD']
+ # }
 
 
 
